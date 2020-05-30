c3e7835c1fe4bce7d4a275e83ccf03d8c9efba97
Mario Semeš <mario.semes@gmail.com>
2020-05-30 12:41:40 +0200

    Added colorama to output

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from psutil import process_iter
 from signal import SIGTERM
 import os
 import time
+import sys
+from colorama import Back, Fore, Style, init
+init()
 
 
 def check_signer_alive(uinput):
@@ -10,9 +13,9 @@ def check_signer_alive(uinput):
             if conns.laddr.port == 55555:
                 if uinput == "kill":
                     proc.send_signal(SIGTERM)
-                    print("Signer proces uspješno ugašen.")
+                    print(f"{Fore.GREEN}Signer proces uspješno ugašen.{Style.RESET_ALL}")
                 elif uinput == "alivecheck":
-                    print("Signer proces AKTIVAN.")
+                    print(f"{Fore.GREEN}Signer proces AKTIVAN.{Style.RESET_ALL}")
 
 
 def find_signer_process_exe():
@@ -40,16 +43,19 @@ def start_signer_app(signer_path):
 
 def main():
     check_signer_alive("kill")
-    print("U potrazi za aplikacijom Signer.exe, molimo pričekajte...")
+    print(f"{Fore.YELLOW}U potrazi za aplikacijom Signer.exe, molimo pričekajte...{Style.RESET_ALL}")
     if find_signer_process_exe():
         start_signer_app(find_signer_process_exe())
     else:
-        print("Signer.exe ne postoji. Jeste li sigurni da je instaliran?")
+        print(f"{Back.RED}Signer.exe ne postoji. Jeste li sigurni da je instaliran?{Style.RESET_ALL}")
 
     time.sleep(5)
     check_signer_alive("alivecheck")
-    print("Slobodno ugasite eID Helper.")
-    input()
+    print(f"{Fore.YELLOW}Slobodno ugasite eID Helper ili će se samostalno ugasiti za 10 sek.{Style.RESET_ALL}")
+    for i in range(10, 0, -1):
+        sys.stdout.write(str(i) + ' ')
+        sys.stdout.flush()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
